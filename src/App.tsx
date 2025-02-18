74cdf9e3707e243fb4dac915885329072615e2c2
Timimah <timimah0117@gmail.com>
2025-02-19 00:57:18 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,160 @@
+import React from "react"
+import SmoothScroll from "./SmoothScroll"
+
+const App: React.FC = () => {
+  return (
+    <SmoothScroll>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
+        odit ex, recusandae dolorum neque exercitationem culpa consectetur
+        molestiae, incidunt sit vero ducimus modi earum quia. Molestiae tenetur
+        enim omnis perferendis. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Temporibus odit ex, recusandae dolorum neque
+        exercitationem culpa consectetur molestiae, incidunt sit vero ducimus
+        modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem ipsum
+        dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Temporibus odit ex,
+        recusandae dolorum neque exercitationem culpa consectetur molestiae,
+        incidunt sit vero ducimus modi earum quia. Molestiae tenetur enim omnis
+        perferendis. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Temporibus odit ex, recusandae dolorum neque exercitationem culpa
+        consectetur molestiae, incidunt sit vero ducimus modi earum quia.
+        Molestiae tenetur enim omnis perferendis. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Temporibus odit ex, recusandae dolorum
+        neque exercitationem culpa consectetur molestiae, incidunt sit vero
+        ducimus modi earum quia. Molestiae tenetur enim omnis perferendis.
+      </div>
+    </SmoothScroll>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "materapro",
+        short_name: "materapro",
+        description: "MateraPro's Web Application",
+        theme_color: "#ffffff",
+      },
+
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
