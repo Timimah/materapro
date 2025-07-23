dc7aeb6ce775157e77426486c31164e2e282de26
Egba Frank <Egbafrank72@gmail.com>
2025-07-23 21:56:27 +0100

    feat(permissions): implement IsOwner permission to restrict edit access to owners only

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -46,3 +46,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         elif hasattr(obj, "supplier"):
             return obj.supplier.user == request.user
         return False
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Allows edit to only the owner.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, "user"):
+            return obj.user == request.user
+        elif hasattr(obj, "supplier"):
+            return obj.supplier.user == request.user
+        return False
