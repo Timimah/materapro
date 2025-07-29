fca6877146dfd055813c6426d5ac3499ebd7134a
Timimah <timimah0117@gmail.com>
2025-07-29 15:21:57 +0100

    feat (assets) : update assets

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -1,12 +1,15 @@
 /// <reference lib="webworker" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching"
+import { NavigationRoute, registerRoute } from "workbox-routing"
 
 declare let self: ServiceWorkerGlobalScope
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting()
 })
 
 // self.__WB_MANIFEST is the default injection point
@@ -16,13 +19,11 @@ precacheAndRoute(self.__WB_MANIFEST)
 cleanupOutdatedCaches()
 
 /** @type {RegExp[] | undefined} */
-let allowlist
+let allowlist: RegExp[] | undefined
 // in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+if (import.meta.env.DEV) allowlist = [/^\/$/]
 
 // to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(
+  new NavigationRoute(createHandlerBoundToURL("index.html"), { allowlist })
+)
